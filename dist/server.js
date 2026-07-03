51524f81a0b7ba3eeaff71d53ca2b7e6c8c81317
Vinicius Freitas <mvinicinhos155@gmail.com>
2026-07-02 22:06:30 -0300

    ...

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("../src/app"));
+const app_1 = __importDefault(require("./app"));
 const port = process.env.PORT || 3000;
 app_1.default.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
